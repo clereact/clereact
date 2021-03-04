cc378d9358649b058c625626218122f685f1f826
Dan Ott <dan@dtott.com>
2021-03-04 08:32:53 -0500

    Make sure meetup api works

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,35 @@ import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
-export default function Home() {
+export async function getStaticProps(context) {
+  try {
+    const meetups = await Promise.all([
+      fetch(
+        'https://api.meetup.com/Cleveland-React/events?desc=false&page=100&status=upcoming',
+      ),
+      fetch(
+        'https://api.meetup.com/Cleveland-React/events?desc=true&page=100&status=past',
+      ),
+    ]);
+    // https://api.meetup.com/Cleveland-React/events?desc=false&page=100&status=upcoming
+    const upcoming = await meetups[0].json();
+    const past = await meetups[1].json();
+    return {
+      props: {
+        meetups: {
+          upcoming,
+          past,
+        },
+      }, // will be passed to the page component as props
+    };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+}
+
+export default function Home({ meetups }) {
+  console.log(meetups);
   return (
     <div className="container">
       <Head>
